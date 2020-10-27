247ff92da45979fc197324576e4a1090c853b8f2
mchaoTechtonic <matthew.chao@techtonic.com>
2020-10-27 18:43:09 -0400

    add promise.race to fix failing tests 4.9 and 4.10

--- src/index.js
+++ src/index.js
@@ -32,3 +32,14 @@ const getBooksAndMoviesPromise = getBooksAndMovies();
 getBooksAndMoviesPromise.then(results => {
     console.log('getBooksAndMoviesPromise', results);
 })
+
+function getBooksOrMovies() {
+    return Promise.race([fetchBooks(), fetchMovies()])
+        .then(results => results)
+        .catch(error => console.log("Error waiting for the promise race", error));
+}
+
+const getBooksOrMoviesPromise = getBooksOrMovies();
+getBooksOrMoviesPromise.then(results => {
+    console.log('getBooksOrMoviesPromise', results);
+})
