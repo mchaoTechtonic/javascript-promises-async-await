dd48f5a38ea20af327aa89c38b9b534e32e0dd83
mchaoTechtonic <matthew.chao@techtonic.com>
2020-10-27 15:15:49 -0400

    add promise structure to pass failing tests 3.6,7,8

--- src/index.js
+++ src/index.js
@@ -5,9 +5,14 @@ const movies = require('./data/movies.json');
 
 export function fetchMovies() {
     const resolveFunction = () => movies;
+    return fetchWithTimeout(1000).then(resolveFunction); //then should receive a function
+    //which does something with a fulfillment value
+    //in this case, resolveFunction doesn't actually need any parameters
 }
 
+const moviePromise = fetchMovies();
 
+moviePromise.then(results=>{console.log(results)});
 
 // export { fetchMovies }; //for some reason this didn't work to export the function above;
 //had to pjt the export keyword instead
