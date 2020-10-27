4087a7ad1954d3f8907976344a51ab11c527a0ce
mchaoTechtonic <matthew.chao@techtonic.com>
2020-10-27 15:23:06 -0400

    Replace fetchMovies promise return with a call to fetch

--- src/services.js
+++ src/services.js
@@ -1,3 +1,10 @@
 export function fetchWithTimeout(delay) {
-    return new Promise(resolve => setTimeout(resolve,delay)); //setTimeout() executes the function resolve, after delay
+    return new Promise(resolve => setTimeout(resolve, delay)); //setTimeout() executes the function resolve, after delay
+}
+
+export function fetchMovies() {
+    return fetch("./data/movies.json")
+        .then(response => response.json()) //this turns the response body into a json object
+        .then(movies => movies)
+        .catch(error => console.log(error));
 }
